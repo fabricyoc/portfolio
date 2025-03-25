7a3587891a534441137011b236e084adf1af9f3d
fabricyo <fazin_c@hotmail.com>
2025-03-25 10:08:40 -0300

    Container e Page404

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Container from '../../components/Container';
 import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
 
@@ -7,23 +8,26 @@ function Home() {
 	return (
 		<>
 			<Header />
-			<section className='container'>
-				<div className='apresentacao'>
-					<p>
-						Olá, sou <br />
-						<span>Fabricyo</span> <br />
-						Dev Full Stack
-					</p>
-					<Link to='/sobre'>
-						<button className='btn btn-red'>
-							Saiba mais
-						</button>
-					</Link>
-				</div>
-				<figure>
-					<img className='img-home' src='/developer-red.svg' alt='Imagem de home' />
-				</figure>
-			</section>
+			<Container>
+				<section className={styles.home}>
+					<div className={styles.apresentacao}>
+						<p>
+							Olá, sou <br />
+							<span>Fabricyo</span> <br />
+							Dev Full Stack
+						</p>
+						<Link to='/sobre'>
+							{/* <button className='btn btn-red'> */}
+							<button className={`${styles.btn} ${styles.btn_red}`}>
+								Saiba mais
+							</button>
+						</Link>
+					</div>
+					<figure>
+						<img className='img-home' src='/developer-red.svg' alt='Imagem de home' />
+					</figure>
+				</section>
+			</Container>
 			<Footer />
 		</>
 	);
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Projetos from "./pages/Projetos";
 import Contatos from "./pages/Contatos";
+import Page404 from "./pages/Page404";
 
 function AppRoutes() {
 	return (
@@ -13,6 +14,7 @@ function AppRoutes() {
 				<Route path="/sobre" element={<Sobre />} />
 				<Route path="/projetos" element={<Projetos />} />
 				<Route path="/contatos" element={<Contatos />} />
+				<Route path="*" element={<Page404 />} />
 
 			</Routes>
 		</BrowserRouter>
