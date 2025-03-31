de0c08796344312d88a594374122135f7dfdac75
fabricyo <fazin_c@hotmail.com>
2025-03-31 10:24:34 -0300

    Consumindo API do GitHub

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,13 +1,49 @@
+import { useEffect, useState } from "react";
 import Card from "../../components/Card";
+import styles from './Projetos.module.css';
 
 function Projetos() {
+
+  const [repositorios, setRepositorios] = useState([]);
+
+  useEffect(() => {
+
+    const buscarRepositorios = async () => {
+      const resposta = await fetch('https://api.github.com/users/fabricyoc/repos');
+      const dados = await resposta.json();
+      setRepositorios(dados);
+    }
+
+    buscarRepositorios();
+
+  }, []);
+
   return (
-    <>
+    <section className={styles.projetos}>
       <h2>Projetos</h2>
-      <Card />
-      <Card />
-      <Card />
-    </>
+      {
+        repositorios.length > 0 ? (
+          <section className={styles.lista}>
+            {
+              repositorios.map(
+                (repo) => (
+                  // sempre que trabalhar com map é necessário a props key
+                  <Card
+                    key={repo.id}
+                    titulo={repo.name}
+                    descricao={repo.description}
+                    url={repo.html_url}
+                  />
+                  
+                )
+              )
+            }
+          </section>
+        ) : (
+          <p>Carregando repositórios...</p>
+        )
+      }
+    </section>
   );
 };
 
